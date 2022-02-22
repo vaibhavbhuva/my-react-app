1df878fd835879cf6e6f86427748d805bfa60e92
Vaibhav Bhuva <vaibhav.bhuva@tarento.com>
2022-02-22 18:21:27 +0530

    Added checkout page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import Header from './components/header/Header.js'
 import Home from './components/home/Home.js';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Checkout from './components/checkout/Checkout.js';
 
 function App() {
   return (
-    <div className='app'>
-      <Header/>
-      <Home/>
-    </div>
+    <Router>
+      <div className='app'>
+        <Header/>
+        <Routes>
+          <Route path="/" element={
+            <Home />
+          } />
+          <Route path="/checkout" element={
+            <Checkout />
+          } />
+          <Route path="*" element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          } />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/checkout/Checkout.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Checkout.css';
+
+export default class Checkout extends Component {
+  render() {
+    return (
+      <div className='checkout'>
+          <div className='checkout__left'>
+
+          </div>
+          <div className='checkout__right'>
+            
+          </div>
+      </div>
+    )
+  }
+}
